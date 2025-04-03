96fa6c7847e83ff182a5e8845a47fb9784a8dc1a
trungntm <trungntm.97@gmail.com>
2025-04-03 21:54:38 +0700

    Test publish

---
+++ packages/starter-test-action/src/index.ts
@@ -0,0 +1,7 @@
+(async () => {
+  try {
+    console.log('MongoAutoConfiguration initialized successfully.');
+  } catch (err) {
+    console.error('Failed to initialize MongoAutoConfiguration:', err);
+  }
+})();
